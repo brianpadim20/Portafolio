f5734b3e5dac7b6eccc3fb54b25ffbad7972d265
Brian Patiño <brianpadim@hotmail.com>
2024-02-06 00:40:57 -0500

    feat: configuración del routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TypingEffectComponent } from './components/typing-effect/typing-effect.component';
+import { MenuComponent } from './components/menu/menu.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, MenuComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,14 +10,14 @@ import { EditComponent } from './components/edit/edit.component';
 import { ErrorComponent } from './components/error/error.component';
 
 export const routes: Routes = [
+    {path:'', component: AboutComponent},
     {path:'sobre-mi', component: AboutComponent},
     {path:'proyectos', component: ProjectsComponent},
     {path:'contacto', component:ContactComponent},
     {path:'crear-proyecto', component: CreateComponent},
     {path:'proyecto/:id', component:DetailComponent},
     {path:'editar-proyecto/:id', component: EditComponent},
-    {path:'error', component:ErrorComponent},
-    {path:'**', component: AboutComponent}
+    {path:'**', component: ErrorComponent}
     
 ];
 
