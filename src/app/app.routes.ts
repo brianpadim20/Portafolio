30bb802ae42c9d33356733237b44296cf1945d93
Brian Patiño <brianpadim@hotmail.com>
2024-02-05 20:27:39 -0500

    feat: creación de rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { ModuleWithProviders, createComponent } from "@angular/core"; 
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { CreateComponent } from './components/create/create.component';
+import { DetailComponent } from './components/detail/detail.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ErrorComponent } from './components/error/error.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'sobre-mi', component: AboutComponent},
+    {path:'proyectos', component: ProjectsComponent},
+    {path:'contacto', component:ContactComponent},
+    {path:'crear-proyecto', component: CreateComponent},
+    {path:'proyecto/:id', component:DetailComponent},
+    {path:'editar-proyecto', component: EditComponent},
+    {path:'**', component: ErrorComponent}
+];
---
+++ src/app/components/typing-effect/typing-effect.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-typing-effect',
+  standalone: true,
+  imports: [],
+  templateUrl: './typing-effect.component.html',
+  styleUrl: './typing-effect.component.css'
+})
+export class TypingEffectComponent {
+
+}
